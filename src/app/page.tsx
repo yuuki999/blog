a4cfd85a1b8ed1140e84873fc6b454a8ec0cea50
Yuuki Itoi <y.itoi@zenplace.co.jp>
2024-02-05 11:15:57 +0900

    動画アップロード検証

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,49 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import React, { useState } from "react";
 
 export default function Home() {
+  const [uploadStatus, setUploadStatus] = useState("");
+
+  // ファイルアップロードハンドラー
+  const handleFileUpload = async (event: any) => {
+    setUploadStatus(""); // ステータスメッセージをリセット
+    const file = event.target.files[0];
+    if (!file) {
+      setUploadStatus("ファイルが選択されていません。");
+      return;
+    }
+    const imageFileName = file.name;
+
+    // FormData オブジェクトを作成し、ファイルを添付
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // fetch API を使用してファイルをバックエンドに送信
+    try {
+      const response = await fetch(`/api/upload?filename=${imageFileName}`, {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        // アップロード成功
+        setUploadStatus('ファイルがアップロードされました。');
+      } else {
+        // アップロード失敗
+        setUploadStatus('ファイルアップロードに失敗しました。');
+      }
+    } catch (error) {
+      console.error('アップロード中にエラーが発生しました:', error);
+      setUploadStatus('アップロード中にエラーが発生しました。');
+    }
+  };
+
   return (
     <main>
       <h1>テストページ</h1>
+      <input type="file" onChange={handleFileUpload} />
+      {uploadStatus && <p>{uploadStatus}</p>}
     </main>
-  )
+  );
 }
